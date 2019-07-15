31d279f5588161fa3eddb785dcc292cda34d3da6
Nick Jones <nkwjones@gmail.com>
2019-07-15 08:48:49 -0400

    update

---
+++ July_2019_Poverty_GP/day_1/first_program.py
@@ -0,0 +1,18 @@
+# a greeting:
+print('Hello world!')
+
+# some variables:
+name = 'Alex'
+days = ['Monday','Tuesday','Wednesday']
+hours = 8
+hobby = "Walk the dog"
+
+print('My name is ', name)
+
+# a loop:
+for day in days:
+    print()
+    print("Plan for", day, ": ")
+    print("Have breakfast")
+    print("Write Python code for", hours, "hours.")
+    print(hobby)
